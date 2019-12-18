ad6eeef436743a2f60ba2be8a821541b7aa68b27
mr-missing-name <59000539+mr-missing-name@users.noreply.github.com>
2019-12-18 10:13:08 +0700

    Create newbie.sh

---
+++ newbie.sh
@@ -0,0 +1,124 @@
+#apa lu liat liat mau recode?
+
+red='\e[1;34m'
+blue='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+pink='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+
+clear
+
+figlet -f slant Tools Termux| lolcat
+
+echo "(+)====================================================(+)" | lolcat
+echo " |                                                      |" | lolcat
+echo " |                Author    :  MR.MISSING NAME          |" | lolcat
+echo " |                Team      :  DrCyberTeam              |" | lolcat
+echo " |                Contac    :  86509433***              |" | lolcat
+echo " |     GUNAKAN TOOLS DENGAN BIJAK HARGAI YG  MEMBUAT    |" | lolcat
+echo " |                                                      |" | lolcat
+echo "(+)====================================================(+)" | lolcat
+date |lolcat
+echo
+echo
+echo "00.GABUNG GRUP WA
+echo "============================" | lolcat
+echo " 1.HACK DDOS
+echo "============================" | lolcat
+echo " 2.INSTALLER TUAN GALLAU
+echo "============================" | lolcat
+echo " 3.HACK TOKEN PLN
+echo "============================" | lolcat
+echo " 4.B-VIRUS
+echo "============================" | lolcat
+echo " 5.HACK IG
+echo "============================" | lolcat
+echo "99.EXIT" 
+echo
+ read -p "PILIH NOMOR :" pill
+
+
+
+
+  if [ $pill = '00' ]
+  then
+
+echo '  https://chat.whatsapp.com/LYVWZcliV22HtjzPTPUEo6'
+echo 'salin Aja Link Diatas Lalu Tempel Ke Google'
+fi
+
+  if [ $pill = ' 1' ]
+  then
+
+clear
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+  if [ $pill = ' 2' ]
+  then
+
+clear
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+  if [ $pill = ' 3' ]
+then
+clear
+echo " Installing Hack Token PLN "| lolcat
+sleep 1
+figlet "token" | lolcat
+figlet "PLN"| lolcat
+figlet "di hack"| lolcat
+figlet "Hargai"| lolcat
+figlet "pekerjaan"| lolcat
+figlet "Mereka"| lolcat
+figlet "goblog"| lolcat
+echo " Done Install Cuk "| lolcat
+fi
+
+  if [ $pill = ' 4' ]
+  then
+
+clear
+pkg install tqdm
+pkg install git
+git clone https://github.com/BlackCyberAnonim/B-VIRUS
+cd B-VIRUS
+python2 B-VIRUS.py
+fi
+
+  if [ $pill = ' 5' ]
+  then
+
+pkg install git
+git clone https://github.com/storiku/instahack
+cd instahack
+bash ig.sh
+fi
+
+  if [ $pill = '99' ]
+  then
+
+echo " Kalau Ada Keslahan / Eror "| lolcat
+sleep 1
+echo " Hubungi"| lolcat
+sleep 1
+echo " 085609433549 "| lolcat
+sleep 1
+echo " Datang Lagi Ya  :* "| lolcat
+sleep 1
+figlet "*_*" | lolcat
+exit
+fi
